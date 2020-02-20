345ec2eeadf552e5c4e7f62a63d4a850bec050a6
ahmad <auepior@gmail.com>
2020-02-20 12:25:21 +0200

    multiply

--- app.js
+++ app.js
@@ -30,11 +30,13 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-
+function multiply(y, x) { //eslint-disable-line
+  var theproduct = y * x;
+    var Mesg = `The product of ${y} and ${x} is ${theproduct}.`;
+return[theproduct, Mesg];
 
 }
-
+testMultiply();
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
 
